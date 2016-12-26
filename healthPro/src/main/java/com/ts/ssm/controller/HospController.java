d62947ba202fed62e227024626db90b532b04808
ts <sheng.tang@ecreditpal.com>
2016-12-26 19:51:11 +0800

    healthpro  submit

---
+++ healthPro/src/main/java/com/ts/ssm/controller/HospController.java
@@ -0,0 +1,36 @@
+package com.ts.ssm.controller;
+
+import com.ts.ssm.model.HospitalDetial;
+import com.ts.ssm.service.HospitalDetialService;
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+
+/**
+ * Created by ts on 2016/12/26.
+ */
+@Controller
+@RequestMapping("/hosp")
+public class HospController {
+
+    private Logger log = Logger.getLogger(DeptController.class);
+    @Resource
+    private HospitalDetialService hospitalDetialService;
+
+    @RequestMapping("/showHosp")
+    public String showHosp(HttpServletRequest request, Model model){
+
+        log.info("查询所有部门信息");
+        List<HospitalDetial> hospitalDetialList = hospitalDetialService.getAllHospitalDetial();
+        model.addAttribute("hospitalDetialList",hospitalDetialList);
+        return "showHosp";
+
+
+    }
+
+}
